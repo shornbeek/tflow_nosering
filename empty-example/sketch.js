f0fdb1abc7e11eeb865e8ff08ddd24daf32af706
Stephen Hornbeek <hornbeeks@gmail.com>
2019-01-11 22:27:55 -0800

    john lenon glasses

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,6 +4,11 @@ let noseX = 0;
 let noseY = 0;
 let eyelX = 0;
 let eyelY = 0;
+let eyelXrr = 0;
+let eyelYrr = 0;
+
+let eyelXa = 0;
+let eyelYa = 0;
  
 function setup() {
   createCanvas(1640, 1480);
@@ -21,10 +26,14 @@ function gotPoses(poses) {
     let nY = poses[0].pose.keypoints[0].position.y;
     let eX = poses[0].pose.keypoints[1].position.x;
     let eY = poses[0].pose.keypoints[1].position.y;
+    let eXa = poses[0].pose.keypoints[2].position.x;
+    let eYa = poses[0].pose.keypoints[2].position.y;
     noseX = lerp(noseX, nX, 0.8);
     noseY = lerp(noseY, nY, 0.4);
     eyelX = lerp(eyelX, eX, 0.8);
     eyelY = lerp(eyelY, eY, 0.4);
+    eyelXa = lerp(eyelX, eXa, 0.8);
+    eyelYa = lerp(eyelY, eYa, 0.7);
   }
 }
 
@@ -39,8 +48,13 @@ function draw() {
 
   fill(155, 110, 110);
   ellipse(noseX, noseY, 5, 5, d);
-  //fill(0,0,255);
-  //ellipse(eyelX, eyelY, 50);
+  fill(10,90,255);
+  ellipse(eyelX, eyelY, 50);
 
+  fill(0,0,255);
+  ellipse(eyelXa, eyelYa, 50);
+  ellipse(eyelXa, eyelYa, 50);
+  line(eyelXa, eyelYa, eyelX, eyelY, 0, 20, 0, 20);
+ 
 
 }
